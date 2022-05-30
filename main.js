ce3124ed505c7cff14c35c63b7ac8ab4d99586ea
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-30 04:17:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let include=(arr, item)=> arr.includes(item)? true:false
+
+/*
+
+8kyu-Enumerable Magic #3 - Does My List Include This?
+
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript
